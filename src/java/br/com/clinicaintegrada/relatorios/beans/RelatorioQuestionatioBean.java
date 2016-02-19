6ae24ee8d5c6b2191817a71d70a4af48fb824213
rtools <bruno.rtools@outlook.com>
2016-02-19 10:41:35 -0200

    Clínica Integrada Ficha Anamnese

--- src/java/br/com/clinicaintegrada/relatorios/beans/RelatorioQuestionatioBean.java
+++ src/java/br/com/clinicaintegrada/relatorios/beans/RelatorioQuestionatioBean.java
@@ -1,15 +1,15 @@
 package br.com.clinicaintegrada.relatorios.beans;
 
-import br.com.clinicaintegrada.principal.DBExternal;
 import br.com.clinicaintegrada.questionario.RespostaLote;
 import br.com.clinicaintegrada.relatorios.dao.RelatorioQuestionarioDao;
-import br.com.clinicaintegrada.seguranca.controleUsuario.SessaoCliente;
 import br.com.clinicaintegrada.utils.Dao;
 import br.com.clinicaintegrada.utils.Jasper;
 import java.io.FileNotFoundException;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -20,7 +20,8 @@ public class RelatorioQuestionatioBean implements Serializable {
     public void print(Integer resposta_lote_id) throws FileNotFoundException {
         RelatorioQuestionarioDao rqd = new RelatorioQuestionarioDao();
         rqd.setIS_QUERY(true);
-        List list = rqd.find(resposta_lote_id);
+        RespostaLote respostaLote = (RespostaLote) new Dao().find(new RespostaLote(), resposta_lote_id);
+        List list = rqd.find(respostaLote);
         // dBExternal.setDatabase(SessaoCliente.get().getIdentifica());
         // dBExternal.setDatabase("ClinicaIntegradaProducao");
         // dBExternal.setPassword("989899");
@@ -28,9 +29,12 @@ public class RelatorioQuestionatioBean implements Serializable {
         Jasper.IS_REPORT_CONNECTION = true;
         Jasper.IS_QUERY_STRING = true;
         Jasper.QUERY_STRING = rqd.getQUERY();
-        RespostaLote respostaLote = (RespostaLote) new Dao().find(new RespostaLote(), resposta_lote_id);
         Jasper.IS_HEADER = true;
         Jasper.TYPE = "default";
-        Jasper.printReports("FICHA_QUESTIONARIO.jasper", respostaLote.getQuestionario().getDescricao(), new ArrayList());
+        Map map = new HashMap();
+        map.put("equipe_nome", respostaLote.getEquipe().getPessoa().getNome());
+        map.put("lancamento", respostaLote.getLancamento());
+        map.put("lote_id", respostaLote.getId());
+        Jasper.printReports("FICHA_QUESTIONARIO.jasper", respostaLote.getQuestionario().getDescricao(), new ArrayList<>(), map);
     }
 }
